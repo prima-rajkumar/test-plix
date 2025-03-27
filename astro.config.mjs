545b47ca66651bb0e9f18f60c8d281235363eee0
Prima-rajkumar <rajkumar.malviya@primathon.in>
2025-03-27 16:32:48 +0530

    this are some of the changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';             // ✅ Latest
+
 
 export default defineConfig({
   output: 'server',
+  adapter: netlify(),
   integrations: [
     preact(),
     tailwind()
